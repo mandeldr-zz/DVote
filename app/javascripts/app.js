b679ff824da1245532bb8d64449874bcb1a9e0b6
Daniel Mandel <mandeldr@mcmaster.ca>
2018-01-03 16:12:43 -0500

    Added a user interface to interact with smart contracts

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -10,8 +10,6 @@ import dvote_artifacts from '../../build/contracts/DVote.json'
 
 // DVote is our usable abstraction, which we'll use through the code below.
 var DVote = contract(dvote_artifacts);
-var provider;
-var dvote;
 
 window.App = {
   start: function() {
@@ -19,16 +17,23 @@ window.App = {
     var self = this;
 
     //set provider
-    DVote.setProvider(provider);
+    DVote.setProvider(web3.currentProvider);
     //get dvote contract instance
+    var dvote;
     DVote.deployed().then(function(instance) {
       // Get democrat vote count
       dvote = instance;
       console.log(dvote)
     })
-    self.getDemCount();
+
+    // self.getDemCount();
+    // self.getRepubCount();
+    // self.voteDemocrat('0x73cf959f59fc91bf636063c7b1f700809d62c58e');
+    // self.voteRepublican('0x2cebd400a9f0fb3baf2f0b887c9e8259deed1317');
+    // self.getBallot('0x73cf959f59fc91bf636063c7b1f700809d62c58e');
   },
   getDemCount: function() {
+    var dvote;
     DVote.deployed().then(function(instance) {
       // Get democrat vote count
       dvote = instance;
@@ -36,11 +41,59 @@ window.App = {
     }).then(function(count) {
       console.log(count.toNumber());
     })
+  },
+  getRepubCount: function() {
+    var dvote;
+    DVote.deployed().then(function(instance) {
+      // Get democrat vote count
+      dvote = instance;
+      return dvote.getRepubCount.call();
+    }).then(function(count) {
+      console.log(count.toNumber());
+    })
+  },
+  voteDemocrat: function(address) {
+    var dvote;
+    DVote.deployed().then(function(instance) {
+      // Get democrat vote count
+      dvote = instance;
+      return dvote.voteDemocrat.call(address, {from: address, gas:3000000});
+    }).then(function(success) {
+      console.log(success);
+    })
+    this.getDemCount();
+  },
+  voteRepublican: function(address) {
+    var dvote;
+    DVote.deployed().then(function(instance) {
+      // Get democrat vote count
+      dvote = instance;
+      return dvote.voteRepublican.call(address, {from: address, gas:3000000});
+    }).then(function(success) {
+      console.log(success);
+    })
+    this.getRepubCount();
+  },
+  getBallot: function(address) {
+    var dvote;
+    DVote.deployed().then(function(instance) {
+      // Get democrat vote count
+      dvote = instance;
+      return dvote.getBallot.call(address);
+    }).then(function(ballot) {
+      console.log(ballot);
+    })
   }
 }
 
 window.addEventListener('load', function() {
   console.log('load')
-  provider = new Web3.providers.HttpProvider("http://localhost:8545");
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
   App.start();
 });
